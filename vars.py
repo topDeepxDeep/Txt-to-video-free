e72517ec382afdeec0902da068d2bbad9aea6630
itsdeep13 <80030947+itsdeep13@users.noreply.github.com>
2025-09-04 12:36:28 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,20 +3,21 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "23480065"))
-API_HASH = environ.get("API_HASH", "32edb7d7fc1523b436109bff8ea061fc")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "3498045"))
+API_HASH = environ.get("API_HASH", "229441a8b355de58ba025be657c82ea3")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8373086447:AAEmSLV-Q85ZtvrX2VCeiTHuYWDCmTOeP7E")
 
-OWNER = int(environ.get("OWNER", "7760196814"))
+OWNER = int(environ.get("OWNER", "6478217960"))
 CREDIT = environ.get("CREDIT", "ᴹᴿ🌿⃝🅺ιʟͥʟͣᴇᷟʀ✍࿐ཽ༵")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '7760196814').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '6478217960').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '7760196814').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '6478217960').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
   
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
